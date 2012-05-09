b8630a5384121ef07f5deff445009b669969b25e
crycore <cry-.-core.docomo.ne.jp>
2012-05-09 11:54:56 +0900

    first commit

---
+++ SLOT_I and E/src/SLOT_WIN/SLOT_IandEActivity.java
@@ -0,0 +1,13 @@
+package SLOT_WIN;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SLOT_IandEActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
